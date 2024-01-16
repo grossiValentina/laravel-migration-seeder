b7b011846e857f70970a9a687b89a4ac96501a12
grossiValentina <grossivalentina435@gmail.com>
2024-01-16 17:40:24 +0100

    add:salvare seeder

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,16 @@ class TrainsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        $train = new Train();
+        $train->azienda = 'Carli';
+        $train->stazione_di_partenza = 'Roma';
+        $train->stazione_di_arrivo = 'Milano';
+        $train->orario_di_partenza = '8:00';
+        $train->orario_di_arrivo = '13:00';
+        $train->codice_treno = '13298';
+        $train->numero_carrozze = 14;
+        $train->cancellato = 0;
+
+        $train->save();
     }
 }
