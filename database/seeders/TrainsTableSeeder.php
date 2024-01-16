1e4d4a741e19ef43c48cdaeec7ae62508bdfe30d
grossiValentina <grossivalentina435@gmail.com>
2024-01-16 18:06:05 +0100

    add: faker

--- database/seeders/TrainsTableSeeder.php
+++ database/seeders/TrainsTableSeeder.php
@@ -5,6 +5,8 @@ namespace Database\Seeders;
 use App\Models\Train;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Faker\Generator as Faker;
+
 
 class TrainsTableSeeder extends Seeder
 {
@@ -13,17 +15,17 @@ class TrainsTableSeeder extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
         $train = new Train();
-        $train->azienda = 'Carli';
-        $train->stazione_di_partenza = 'Roma';
-        $train->stazione_di_arrivo = 'Milano';
-        $train->orario_di_partenza = '8:00';
-        $train->orario_di_arrivo = '13:00';
-        $train->codice_treno = '13298';
-        $train->numero_carrozze = 14;
-        $train->cancellato = 0;
+        $train->azienda = $faker->randomElement(['Italo', 'Trenitalia']);
+        $train->stazione_di_partenza = $faker->city();
+        $train->stazione_di_arrivo = $faker->city();
+        $train->orario_di_partenza = $faker->time();
+        $train->orario_di_arrivo = $faker->time();
+        $train->codice_treno = $faker->ean8();
+        $train->numero_carrozze = $faker->randomDigitNotNull();
+        $train->cancellato = $faker->boolean();
 
         $train->save();
     }
